181d6abf5f8e8757854859aef463c9a5057e7099
Sonja Parsell <hello@sparsell.com>
2020-03-06 15:09:29 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-  puts "   |   |   "
+  puts " #{board[0]}  |   |   "
   puts "-----------"
-  puts "   | X |   "
+  puts "   | #{board[4]} |   "
   puts "-----------"
   puts "   |   |   "
 end
